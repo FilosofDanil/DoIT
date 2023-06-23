731ec4849d3015022f6c8f12d0926d1119e07c76
FilosofDanil <serhio3347@gmail.com>
2023-06-23 19:32:14 +0200

    Added new wallpaper for a habit page Redesigned showing of an habits entity. Added creation button

--- Frontend/front/src/components/site-content/Habits.vue
+++ Frontend/front/src/components/site-content/Habits.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main-gradient-1"></div>
-  <img class="insane_image" src="https://images.wallpaperscraft.com/image/single/dark_forest_road_167292_3840x2160.jpg">
+  <img class="insane_image" src="https://images.pexels.com/photos/512229/pexels-photo-512229.jpeg?cs=srgb&dl=pexels-pixabay-512229.jpg&fm=jpg">
   <h1 class="main-text bolder cool-text-color">Improve your Habitats</h1>
   <h2 class="current">Your currently habits, which have been worked on:</h2>
   <div class="habits_table">
@@ -8,14 +8,32 @@
       <h1 class="bolder cool-text-color white-color">{{ habit.name }}</h1>
       <h1 v-if="habit.completed" class="bolder cool-text-color white-color">Current status(completed): Yes</h1>
       <h1 v-if="!habit.completed" class="bolder cool-text-color white-color">Current status(completed): No</h1>
-      <div class="tracks" v-for="track in getAllHabitsTracks(habit.id)">
-        <i v-if="track.status==='before'&&track.marked===true" class="fa-solid fa-circle circle_1"></i>
-        <i v-if="track.status==='before'&&track.marked===false" class="fa-solid fa-circle circle_4"></i>
-        <i v-on:click="unmark(track.id)" v-if="track.status==='today'&&track.marked===true"
-           class="fa-solid fa-circle circle_1 today"></i>
-        <i v-on:click="mark(track.id)" v-if="track.status==='today'&&track.marked===false"
-           class="fa-solid fa-circle circle_2 today"></i>
-        <i v-if="track.status==='after'" class="fa-solid fa-circle circle_3"></i>
+      <button @click="showHistory(habit.id)" class="form-button check_history">Show History</button>
+      <i class="edit fa-solid fa-edit"></i>
+      <div class="today_markup">
+        <h2 class="bolder cool-text-color white-color">Today duty markup</h2>
+      </div>
+      <div v-if="getFromMap(habit.id)" class="track-block">
+        <div class="tracks" v-for="track in getAllHabitsTracks(habit.id)">
+          <i v-if="track.status==='before'&&track.marked===true" class="fa-solid fa-circle circle_1"></i>
+          <i v-if="track.status==='before'&&track.marked===false" class="fa-solid fa-circle circle_4"></i>
+          <i v-on:click="unmark(track.id)" v-if="track.status==='today'&&track.marked===true"
+             class="fa-solid fa-circle circle_1 today"></i>
+          <i v-on:click="mark(track.id)" v-if="track.status==='today'&&track.marked===false"
+             class="fa-solid fa-circle circle_2 today"></i>
+          <i v-if="track.status==='after'" class="fa-solid fa-circle circle_3"></i>
+        </div>
+      </div>
+
+
+      <div class="creation">
+        <form class="login-form" action="">
+          <label class="labels">
+            <p class="input-name">Name</p>
+            <input id="email" class="loh-input input">
+          </label>
+          <button @click="" class="form-button create_but">Create</button>
+        </form>
       </div>
     </div>
   </div>
@@ -29,11 +47,14 @@ export default {
   name: "Habits",
   data: () => ({
     habits: [],
-    tracks: []
+    tracks: [],
+    track_map: new Map(),
   }),
   methods: {
     getAllHabits() {
-      HabitsService.getAll().then((response) => this.habits = response.data)
+      HabitsService.getAll().then((response) => {this.habits = response.data
+        this.habits.forEach(habit => this.track_map.set(habit.id, false))
+      })
     },
     getAllHabitsTracks(id) {
       HabitsService.getAllTracks(id).then((response) => this.tracks = response.data)
@@ -48,6 +69,16 @@ export default {
       HabitsService.unmark(id).then(() => {
         this.$router.push('/habits')
       })
+    },
+    getFromMap(id) {
+      return this.track_map.get(id)
+    },
+    showHistory(id){
+      if (this.track_map.get(id)) {
+        this.track_map.set(id, false)
+      } else {
+        this.track_map.set(id, true)
+      }
     }
   },
 
@@ -78,7 +109,7 @@ export default {
   color: white;
   font-size: 42px;
   z-index: 1;
-  top: 20.5%;
+  top: 24vh;
   left: 5vh;
 }
 
@@ -109,10 +140,51 @@ export default {
   color: darkred;
 }
 
+.track-block{
+  max-width: 80vh;
+  left:70vh;
+}
+
+.today_markup{
+  bottom: 7vh;
+  max-width: 50vh;
+}
+
 .tracks {
   display: inline-grid;
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: repeat(1.2, 1fr);
   grid-gap: 1em;
+}
+
+.main-gradient-1 {
+  position: absolute;
+  top: 3.8rem;
+  width: 100vw;
+  height: 130vh;
+  background: linear-gradient(180deg, #090B13 0%, rgba(9, 11, 19, 0.2) 100%);
+  opacity: 60%;
+  z-index: 0;
+}
+.creation{
+  top:27.5vh;
+  min-height: 20vh;
+}
+
+.create_but{
+  top:20vh;
+  right:40vh;
+}
+
+.check_history{
+  bottom:6.5vh;
+  left:80vh;
+}
+
+.edit{
+  color:white;
+  left:150vh;
+  font-size: 2.5em;
+  bottom:5vh;
 }
 </style>
